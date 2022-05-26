3a9ce0d8e6b6743e38cf73adefd01926103b0791
26eldrpau <99837840+26eldrpau@users.noreply.github.com>
2022-05-26 09:21:03 -0400

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,29 +1,7 @@
-if (!blooketUtility.prefs.test) {
-  blooketUtility.prefs.test = {
-    title: "Gold Quest",
-    description: "Gold Quest Cheat",
-    value: {
-      testBool: {
-        title: "Test Boolean",
-        description: "Test boolean for the Blooket Utility",
-        value: true,
-      },
-      testGroup: {
-        title: "Test Group",
-        description: "Test group for the Blooket Utility",
-        value: {
-          testBool2: {
-            title: "Test 2",
-            description: "Yet another test",
-            value: false,
-          },
-          testBool3: {
-            title: "Test 3",
-            description: "Yet another test",
-            value: true,
-          },
-        },
-      },
-    },
+     if (!blooketUtility.prefs.goldquest) {
+  blooketUtility.prefs.goldquest = {
+    title: "goldquest",
+    description: "cheat for gold quest",
+    value: true,
   };
 }
